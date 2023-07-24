ed92860a17c0bb1387ab7e40dcdbd73bfdb09e04
Samuel Rosário <willisdev5@gmail.com>
2023-07-23 22:40:42 -0300

    barbie fast project challange 24h

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,21 @@
+//links
+const noticias = document.querySelector("#noticias")
+const filmesSeries = document.querySelector("#filmesSeries")
+
+
 // seções 
+const secNoticias = document.querySelector(".secNoticias")
+const secFilmesSeries = document.querySelector(".secFilmesSeries")
 
 
+noticias.addEventListener('click', () =>{
+    secNoticias.setAttribute('style', 'display:flex;')
+    secFilmesSeries.setAttribute('style', 'display:none;')
+
+})
+
+filmesSeries.addEventListener('click', () => {
+    secNoticias.setAttribute('style', 'display:none;')
+    secFilmesSeries.setAttribute('style', 'display:flex;')
+   
+})
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,3 @@
-const secNoticias = document.querySelector(".secNoticias")
-const secFilmesSeries = document.querySelector(".secFilmesSeries")
-
 const allNoticias = [{
     url: "./src/imgNoticias/ingressosparabarbie.jpg",
     newsType: "Filme Barbie Ingressos",
@@ -16,7 +13,7 @@ const allNoticias = [{
 }, {
     url: "./src/imgNoticias/barbieEKneconzinheiro.png",
     newsType: "Frete gratis",
-    msg: "Atenção!!!! Frete gratis na compra do Ken e Barbie cozinheiros."
+    msg: "Atenção!!! Frete gratis na compra do Ken e Barbie cozinheiros."
 }]
 
 secNoticias.innerHTML = allNoticias.map(noticias => {
@@ -31,46 +28,53 @@ secNoticias.innerHTML = allNoticias.map(noticias => {
 }).join('');
 
 
+// botoes
+const filmesBtn = document.querySelector("#filmesBtn")
+const seriesBtn = document.querySelector("#seriesBtn")
 
 // Filme e serie sections
 const filmes = document.querySelector("#filmes")
 const series = document.querySelector("#series")
 
 const allFilmes = [{
-    imgFilmes: "./src/imgMovieBarbie/movie2023.webp",
+    imgFilmes: "/src/imgMovieBarbie/movie2023.webp",
     nameFilmes: "Barbie 2023"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/BarbieReinodasFadas.webp",
+    imgFilmes: "/src/imgMovieBarbie/BarbieReinodasFadas.webp",
     nameFilmes: "Segredo das Fadas"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/magiadepegaso.png",
+    imgFilmes: "/src/imgMovieBarbie/magiadepegaso.png",
     nameFilmes: "Magia de Pégaso"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/oCastelodeDiamante.webp",
+    imgFilmes: "/src/imgMovieBarbie/oCastelodeDiamante.webp",
     nameFilmes: "O Castelo de diamante"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/BarbiePrincesaIlha.jpg",
+    imgFilmes: "/src/imgMovieBarbie/BarbiePrincesaIlha.jpg",
     nameFilmes: "Barbie e a Princesa da Ilha"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/BarbiePortalSecreto.jpg",
+    imgFilmes: "/src/imgMovieBarbie/BarbiePortalSecreto.jpg",
     nameFilmes: "Barbie e o Portal Secreto"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/BarbiePrincesaPlebeia.jpg",
+    imgFilmes: "/src/imgMovieBarbie/BarbiePrincesaPlebeia.jpg",
     nameFilmes: "Barbie e a Princesa e a Plebeia"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/aventuraNaEstrelas.jpg",
+    imgFilmes: "/src/imgMovieBarbie/aventuraNaEstrelas.jpg",
     nameFilmes: "Aventura Nas Estrelas"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/Barbie12PrincesasBailarinas.webp",
+    imgFilmes: "/src/imgMovieBarbie/Barbie12PrincesasBailarinas.webp",
     nameFilmes: "E as 12 Princesas Bailarinas"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/BarbieOLagodosCisnes.jpg",
+    imgFilmes: "/src/imgMovieBarbie/BarbieOLagodosCisnes.jpg",
     nameFilmes: "Barbie em O Lago dos Cisnes"
 }, {
-    imgFilmes: "./src/imgMovieBarbie/princessapopstar.jpg",
+    imgFilmes: "/src/imgMovieBarbie/princessapopstar.jpg",
     nameFilmes: "Princessa e Popstar"
 }]
 
+filmesBtn.addEventListener('click', () => {
+    filmes.setAttribute('style', 'display:flex;')
+    series.setAttribute('style', 'display:none;')
+})
 filmes.innerHTML = allFilmes.map(filmes => {
     return `
     
@@ -82,32 +86,37 @@ filmes.innerHTML = allFilmes.map(filmes => {
 }).join('');
 
 const allSeries = [{
-    imgSeries: "./src/imgSerieBarbie/LifeintheDreamhouse.jpg",
+    imgSeries: "/src/imgSerieBarbie/LifeintheDreamhouse.jpg",
     nameSeries: "Life in the Dreamhouse"
 }, {
-    imgSeries: "./src/imgSerieBarbie/dreamhouseAdventures.jpeg",
+    imgSeries: "/src/imgSerieBarbie/dreamhouseAdventures.jpeg",
     nameSeries: "Dreamhouse Adventures"
 }, {
-    imgSeries: "./src/imgSerieBarbie/DreamhouseParty.jpg",
+    imgSeries: "/src/imgSerieBarbie/DreamhouseParty.jpg",
     nameSeries: "Dreamhouse Party"
 }, {
-    imgSeries: "./src/imgSerieBarbie/Dreamtopia.jpg",
+    imgSeries: "/src/imgSerieBarbie/Dreamtopia.jpg",
     nameSeries: "Dreamtopia"
 }, {
-    imgSeries: "./src/imgSerieBarbie/ItTakesTwo.jpg",
+    imgSeries: "/src/imgSerieBarbie/ItTakesTwo.jpg",
     nameSeries: "It Takes Two"
 }, {
-    imgSeries: "./src/imgSerieBarbie/PrincessesPower.jpg",
+    imgSeries: "/src/imgSerieBarbie/PrincessesPower.jpg",
     nameSeries: "Princesses Power"
 }]
 
+seriesBtn.addEventListener('click', () => {
+    series.setAttribute('style', 'display:flex;')
+    filmes.setAttribute('style', 'display:none;')
 
-series.innerHTML = allSeries.map(series => {
-    return `
+    series.innerHTML = allSeries.map(series => {
+        return `
+    
             <div class="filmesSeries">
                 <img src="${series.imgSeries}" alt="filme ou serie">
                 <p>${series.nameSeries}</p>
             </div>
 
     `
-}).join('');
+    }).join('');
+})
